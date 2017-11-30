a2ff2575d78ef50f9f0ab1a68657f954f8addf83
Peter Lee <dlee14@stuy.edu>
2017-11-30 15:36:26 -0500

    works up to fork

--- shell.c
+++ shell.c
@@ -13,38 +13,33 @@
 char ** parse_args(char * line);
 
 int main(){
-  // char line[100] = "ls -a -l";
-  // char ** args = parse_args(line);
-  // execvp(args[0], args);
-
-  FILE *f1 = fopen("test.txt", "r");
-  int i;
-  fseek(f1, 0, SEEK_END);
-  int inputsize = ftell(f1); //its about text's size
-  char input1[inputsize];
-  fseek(f1,0,SEEK_SET);
-  printf("%d\n", inputsize);
-  for(i = 0; i < inputsize; i++) {
-    fscanf(f1, "%c", &input1[i]);
-    printf("%c", input1[i]);
+  printf("LiLee shell initiated\n");
+  char line[100];
+  fgets(line, 100, stdin);
+  printf("line is: %s\n", line);
+  char ** args = parse_args(line);
+  pid_t pid = fork();
+  if (!pid) {
+     execvp(args[0], args);
+  } else {
+    
   }
-
-  printf("%c", input1[5]);
+  printf("error: %s\n", strerror(errno));
   return 0;
 }
 
 char ** parse_args(char * line){
-  char ** s1 = (char **)calloc(6, sizeof(line));
+  char ** s1 = (char **)malloc(sizeof(line));
 
   int i = 0;
+
+  char * pos;
   while(line){
     s1[i] = strsep(&line, " ");
+    if ((pos=strchr(s1[i], '\n')) != NULL) {
+      *pos = '\0';
+    }
     i++;
-  }
-
-  int x;
-  for(x = 0; x < 6; x++){
-    printf("arguments: %s\n", s1[x]);
   }
 
   return s1;
