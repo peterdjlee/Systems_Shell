53b09b9591a92de9d8c66cb068c93a38ff307c52
Kevin Li <kevinli@dwightshrute.home>
2017-12-02 18:41:21 -0500

    added cd and exit

--- shell.c
+++ shell.c
@@ -10,30 +10,12 @@
 #include <errno.h>
 #include <string.h>
 
-char ** parse_args(char * line);
-
-int main(){
-  printf("LiLee shell initiated\n");
-  char line[100];
-  fgets(line, 100, stdin);
-  printf("line is: %s\n", line);
-  char ** args = parse_args(line);
-  pid_t pid = fork();
-  if (!pid) {
-     execvp(args[0], args);
-  } else {
-    
-  }
-  printf("error: %s\n", strerror(errno));
-  return 0;
-}
-
 char ** parse_args(char * line){
   char ** s1 = (char **)malloc(sizeof(line));
 
   int i = 0;
-
   char * pos;
+
   while(line){
     s1[i] = strsep(&line, " ");
     if ((pos=strchr(s1[i], '\n')) != NULL) {
@@ -43,4 +25,48 @@ char ** parse_args(char * line){
   }
 
   return s1;
+}
+
+int main(){
+  printf("---LiLee shell initiated!---\n");
+
+  //reading in stdin and parsing
+  char line[100];
+  fgets(line, 100, stdin);
+  printf("line is: %s\n", line);
+  char ** args = parse_args(line);
+
+  int f;
+  f = fork();
+
+  if (f == 0) {
+      //child process
+      printf("Child is running!\n");
+
+      //don't do it if its exit or cd
+      if(!strcmp(args[0], "exit") || !strcmp(args[0], "cd")) {
+        exit (0);
+      }
+
+      //exec
+      execvp(args[0], args);
+  }
+
+  else {
+    int status;
+    wait(&status);
+    printf("Parent is running!\n");
+
+    if(!strcmp(args[0], "exit")){
+      exit(0);
+    }
+    if(!strcmp(args[0], "cd")) {
+      int works;
+      works = chdir(args[1]);
+      printf("runs? %d\n", works);
+    }
+    printf("Parent Done!\n");
+  }
+  //printf("error: %s\n", strerror(errno));
+  return 0;
 }
