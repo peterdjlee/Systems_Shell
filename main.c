a8ed6c3aafdc1f655c1f0963c9f2ae872dca9eb7
Peter Lee <dlee14@stuy.edu>
2017-12-04 02:43:48 -0500

    finishing up documentation and comments and minor tweaks

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 int main(){
   printf("\nWelcome to Li-Lee shell. Waiting for you to type...\n");
   printf("Note: You MUST NOT use whitespace when using ';'\n");
-  printf("Type 'exit' to exit.\n");
-  printf("Type 'magic' to hide/show your current working directory.\n\n");
-
+  printf("Note: Type 'exit' to exit.\n");
+  printf("Note: Type 'magic' to hide/show your current working directory.\n\n");
   int cwd_on = 1;
+
   while (1) {
+    // All for the magic
     if (cwd_on) {
       char cwd[1024];
       if (getcwd(cwd, sizeof(cwd)) != NULL) {
@@ -21,6 +22,7 @@ int main(){
     fgets(line, 100, stdin);
     char ** args = parse_args(line, ';');
 
+    // All for the magic
     if(!strcmp(line, "magic")) {
       if (cwd_on == 0) {
         cwd_on = 1;
@@ -30,21 +32,26 @@ int main(){
       continue;
     }
 
+    if(!strcmp(line, "exit")) {
+      printf("Aw... leaving already?\n\n");
+      printf("Bye!\n\n");
+      exit(0);
+    }
+
     int i = 0;
     int status;
+    // Parse each set of commands and execute
     while (args[i]) {
       char ** cmd = parse_args(args[i], ' ');
-      exec_one(cmd);
-      wait(&status);
-      if(!strcmp(cmd[0], "exit")){
-        printf("Quitting...\n");
-        exit(0);
-      }
+      // If directory for cd doesn't exist, print error
       if(!strcmp(cmd[0], "cd")) {
-        if (chdir(cmd[1]) == -1) {
+        if (chdir(cmd[1]) == -1 && cmd[1] != NULL) {
           printf("-bash: cd: %s: %s\n", cmd[1], strerror(errno));
         }
       }
+      exec_one(cmd);
+      // Be a good parent and wait for child
+      wait(&status);
       i++;
     }
   }
--- shell.c
+++ shell.c
@@ -10,21 +10,22 @@
   Return Value:
     A char ** which consists of the parsed strings
 */
-char ** parse_args(char * line, char delimiter){
+char ** parse_args(char * line, char delimiter) {
   int size = 6;
   char ** s1 = (char **)malloc(size * sizeof(char *));
 
   int i = 0;
   char * pos;
-
+  // Parse the string using the given delimiter
   while(line){
     s1[i] = strsep(&line, &delimiter);
-    // If newline is there make it null
+    // If there is newline, make it null
     if ((pos=strchr(s1[i], '\n')) != NULL) {
       *pos = '\0';
     }
     i++;
   }
+  // Put a terminating null pointer at the end
   s1[i] = 0;
   return s1;
 }
@@ -32,14 +33,14 @@ char ** parse_args(char * line, char delimiter){
 /*
   void exec_one(char ** args)
   Description:
-    Forks off and the child process
+    Forks off a child process and the child process calls execvp to execute the given command
   Arguments:
-    char * line -> A pointer to the original string
-    char delimiter -> The character that is going to be used to parse the string
+    char ** args -> Command line arguments that should be executed
   Return Value:
-    A char ** which consists of the parsed strings
+    N/A
 */
 void exec_one(char ** args) {
+  // Fork off a child process
   int f = fork();
   if (f == 0) {
     // Child process
@@ -50,6 +51,7 @@ void exec_one(char ** args) {
     }
     int i = 0;
     i = execvp(args[0], args);
+    // If there was something went wrong, print out the error
     if (i < 0) {
       printf("-bash: %s: %s \n", args[0], strerror(errno));
     }
