e924042678c5d07c3c6dd177e76e6e62ddc72e37
Kevin Li <kevinli@dwightshrute.home>
2017-12-04 03:53:18 -0500

    added redirection in

--- shell.c
+++ shell.c
@@ -30,6 +30,29 @@ char ** parse_args(char * line, char delimiter) {
   return s1;
 }
 
+void redir_in(char ** args){
+    printf("this happened!\n");
+    int cmd, file, i;
+    i = 0;
+    //printf("%s\n",args[3]);
+    while (args[i]) {
+      if (strcmp(args[i],">") == 0){
+        break;
+      }
+      i++;
+    }
+    printf("this is i: %d\n", i);
+    file = i + 1;
+    int fd, b, c;
+    fd = open(args[file], O_CREAT | O_WRONLY | O_TRUNC | O_RDONLY, 0644);
+    b = dup(STDOUT_FILENO);
+    c = dup2(fd, STDOUT_FILENO);
+    args[i]=0;
+    exec_one(args);
+    dup2(b,c);
+    exit(0);
+}
+
 /*
   void exec_one(char ** args)
   Description:
@@ -49,9 +72,22 @@ void exec_one(char ** args) {
     if(!strcmp(args[0], "exit") || !strcmp(args[0], "cd")) {
       exit(0);
     }
+    if (args[1]){
+      int i = 0;
+      while (args[i]) {
+        if (strchr(args[i], '>') != NULL) {
+          printf("IT WAS FOUND!! \n");
+          redir_in(args);
+          break;
+        }
+        i++;
+      }
+    }
     int i = 0;
     i = execvp(args[0], args);
+
     // If there was something went wrong, print out the error
+
     if (i < 0) {
       printf("-bash: %s: %s \n", args[0], strerror(errno));
     }
